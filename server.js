146beff8b5ce49555e7f57fbb37689100d690300
MartinAtanasovWork <martiatanasof@gmail.com>
2025-02-14 12:17:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const querystring = require('querystring');
+const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -18,7 +19,19 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify({ message: 'Post request received', data: body }));
         });
-    } else {
+    }  else if (req.method === 'GET') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Error loading index.html');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
+        });
+    }else {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Hello, World!');
